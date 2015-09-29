746da6665b8de10ca2b12235cdc7b0235419ec0d
Artem Aminov <artemaminov@gmail.com>
2015-09-29 18:07:56 +0600

    fix ThreeDTemplate model :cover where relation

--- app/models/three_d_template.rb
+++ app/models/three_d_template.rb
@@ -1,6 +1,6 @@
 class ThreeDTemplate < ActiveRecord::Base
   belongs_to :block
-  has_one :cover, -> { where attachable_type: 'Cover' }, as: :attachable, class_name: Attachment, foreign_key: :attachable_id, dependent: :delete
+  has_one :cover, -> { where attachable_type: 'ThreeDTemplate' }, as: :attachable, class_name: Attachment, foreign_key: :attachable_id, dependent: :delete
 
   accepts_nested_attributes_for :cover
 
